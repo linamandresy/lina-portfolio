b8205c03711ed1e92528d717f2289fae4a893d4f
Lina Mandresy <mandresyrab@gmail.com>
2025-04-07 16:16:34 -0400

    Add slug to projects

--- cms/schemaTypes/projectType.ts
+++ cms/schemaTypes/projectType.ts
@@ -1,49 +1,72 @@
 import { defineField, defineType } from "sanity";
+import { filter } from "sanity/migrate";
 
 export const projectType = defineType({
-    name:'project',
-    type:'document',
-    title:'Project',
-    fieldsets:[
-        {name:'duration',options:{columns:2}}
+    name: 'project',
+    type: 'document',
+    title: 'Project',
+    fieldsets: [
+        { name: 'duration', options: { columns: 2 } }
     ],
-    fields:[
+    fields: [
         defineField({
-            name:'projectTitle',
-            type:'string',
-            validation : (rule)=>rule.required()
-        }),        
-        defineField({
-            name:'from',
-            fieldset:'duration',
-            type:'date'
+            name: 'projectTitle',
+            type: 'string',
+            validation: (rule) => rule.required()
         }),
         defineField({
-            name:'to',
-            type:'date',
-            fieldset:'duration'
+            name:'slug',
+            type:'slug',
+            options:{
+                source:'projectTitle'
+            },
+            validation:(rule)=>rule.required()
         }),
         defineField({
-            name:'image',
-            type:'image'
+            name: 'from',
+            fieldset: 'duration',
+            type: 'date'
         }),
         defineField({
-            name:'description',
-            type:'array',
-            of:[{
-             type:'block'   
+            name: 'to',
+            type: 'date',
+            fieldset: 'duration'
+        }),
+        defineField({
+            name: 'image',
+            type: 'image'
+        }),
+        defineField({
+            name: 'description',
+            type: 'array',
+            of: [{
+                type: 'block'
             }]
         }),
         defineField({
-            name:'actor',
-            type:'reference',
-            to:{type:'profile'}
+            name: 'actor',
+            type: 'reference',
+            to: { type: 'profile' }
         }),
         defineField({
-            name:'technologies',
-            title:'Technologies',
-            type:'array',
-            of:[{type:'reference',to:[{type:'technology'}]}]
+            name: 'technologies',
+            title: 'Technologies',
+            type: 'array',
+            of: [{ 
+                type: 'reference', 
+                to: [{ type: 'technology' }] ,
+                options:{
+                    filter:({document})=>{
+                        const selectedTech = (document?.technologies||[]).map(
+                            (ref:any)=>ref._ref
+                        )
+                        return {
+                            filter:'!(_id in $selected)',
+                            params:{selected:selectedTech}
+                        }
+                    }
+                }
+            }]
         })
 
     ]
