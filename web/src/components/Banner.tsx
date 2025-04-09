39f658a0ba095227f3ceb20a0afd78b0ee05a382
Lina Mandresy <mandresyrab@gmail.com>
2025-04-09 17:08:51 -0400

    Banner first design

--- web/src/components/Banner.tsx
+++ web/src/components/Banner.tsx
@@ -1,22 +1,21 @@
 import style from "@/style/banner.module.css";
 
-export default function Banner(props:{
+export default function Banner(props: {
   title: string;
   subtitle: string;
   imageUrl: string;
 }) {
   return (
-    <div className={style.banner} style={{backgroundImage: `url(${props.imageUrl})`}}>
-        <div className={style.overlay}>
-            <div className={style.text}>
-                <h1>{props.title}</h1>
-                <h2>{props.subtitle}</h2>
-            </div>
+    <div className={style.banner} >
+      <img src={props.imageUrl} alt={props.title} className={style.image} />
+      <div className={style.text}>
+        <div className={style.title}>
+          <span>Hi, I'm </span>
+          <h1> {props.title}</h1>
         </div>
+        <span>{props.subtitle}</span>
+      </div>
     </div>
-
-
-            
   );
 
 }
