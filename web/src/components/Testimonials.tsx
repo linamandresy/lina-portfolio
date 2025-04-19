235eb2a2cbf992eb27b692875574bb196523e1bb
Lina Mandresy <mandresyrab@gmail.com>
2025-04-19 19:28:03 -0400

    Dynamize Testimonials Section

--- web/src/components/Testimonials.tsx
+++ web/src/components/Testimonials.tsx
@@ -1,26 +1,21 @@
 'use client';
+import { client } from "@/sanity/client";
 import { Carousel, Typography } from "@material-tailwind/react";
+import { defineQuery, PortableText } from "next-sanity";
 
-const data: { imageUrl: string, title: string, description: string }[] = [{
-  imageUrl: 'https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80',
-  title: 'Mohesh',
-  description: 'Lina a ete un tres bon stagiaire, il a su s`adapter a l equipe et a la culture de l entreprise. Elle a su faire preuve d autonomie et de curiosite tout au long de son stage. Elle a su s adapter aux besoins de l entreprise et a su se former sur les technologies utilisees. Elle a su travailler en equipe et a su s integrer dans l equipe. Elle a su faire preuve d autonomie et de curiosite tout au long de son stage. Elle a su s adapter aux besoins de l entreprise et a su se former sur les technologies utilisees. Elle a su travailler en equipe et a su s integrer dans l equipe.'
-},
-{
-  imageUrl: 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
-  title: 'Naina',
-  description: 'Lina a ete un tres bon stagiaire, il a su s`adapter a l equipe et a la culture de l entreprise. Elle a su faire preuve d autonomie et de curiosite tout au long de son stage. Elle a su s adapter aux besoins de l entreprise et a su se former sur les technologies utilisees. Elle a su travailler en equipe et a su s integrer dans l equipe. Elle a su faire preuve d autonomie et de curiosite tout au long de son stage. Elle a su s adapter aux besoins de l entreprise et a su se former sur les technologies utilisees. Elle a su travailler en equipe et a su s integrer dans l equipe.'
-}];
+const QUERY_TESTIMONIALS = defineQuery(`*[ _type == 'testimonials' ]{...}`);
 
-export default function Testimonials() {
 
-  
+
+
+export default async function Testimonials() {
+  const testimonials = await client.fetch(QUERY_TESTIMONIALS);
   return (
     <Carousel className="rounded-xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} >
-      {data.map((item, index) => (
+      {testimonials.map((item:any, index:any) => (
         <div className="relative h-full w-full" key={index}>
           <img
-            src={item.imageUrl}
+            src='https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80'
             alt="image 1"
             className="h-96 w-full object-cover"
           />
@@ -32,7 +27,7 @@ export default function Testimonials() {
                 className="mb-4 text-3xl md:text-4xl lg:text-5xl "
                 placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
               >
-                {item.title}
+                {item.author}
               </Typography>
               <Typography
                 variant="lead"
@@ -40,7 +35,7 @@ export default function Testimonials() {
                 className="mb-12 opacity-80"
                 placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
               >
-                {item.description}
+                <PortableText value={item.feedback}  />
               </Typography>
 
             </div>
