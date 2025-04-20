52af0873644ebe560daabccf9408b02ce31e56f1
Lina Mandresy <mandresyrab@gmail.com>
2025-04-19 20:24:14 -0400

    Remove see more button

--- web/src/components/RoleCard.tsx
+++ web/src/components/RoleCard.tsx
@@ -6,9 +6,9 @@ export default function RoleCard({role,description,imageUrl}:{ role:string|undef
             <img src={imageUrl} alt={role} className={styles.image}/>
             <h4 className={styles.role}>{role}</h4>
             <p className={styles.description}>{description}</p>
-            <button className={styles.button} >
+            {/* <button className={styles.button} >
                 View More
-            </button>
+            </button> */}
         </div>
     );
 }
